ed0fc5df1618f6eee9e350a9bad41478c9da8558
liulrdkc <35749870+liulrdkc@users.noreply.github.com>
2018-01-27 12:41:41 +0800

    Update digest.py

--- digest.py
+++ digest.py
@@ -7,17 +7,3 @@ class Digest(bytes):
         
     def __repr__(self) -> str:
         return binascii.hexlify(self).decode('ascii')
-        
-    @classmethod
-    def sha256(*var)->Digest:
-        ctx = hashlib.sha256()
-        for val in vals:
-            if isinstance(val, str):
-                ctx.update(val.encode('utf8'))
-            elif isinstance(val, int):
-                ctx.update(val.to_bytes(16, 'big'))
-            elif hasattr(val, '__digest__'):
-                ctx.update(val.__digest__())
-            else:
-                ctx.update(val)
-        return Digest(ctx.digest())
